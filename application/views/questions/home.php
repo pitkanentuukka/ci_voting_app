a3d1223de8ba771045c5228273d9a8ebf6105f2a
tuukka <tuukka@thinkpad>
2020-04-07 17:02:13 +0300

    question form validation seems to work now

--- application/views/questions/home.php
+++ application/views/questions/home.php
@@ -30,7 +30,7 @@
 <div class="form-group mr-2">
 	<label for "question" >Question</label>
 	
-	<input type="text" name="question" id="question" class="form-control" placeholder="add a question" required>
+	<input type="text" name="question" id="question" class="form-control" placeholder="add a question" required/>
 </div>
 <div class="form-group mr-2">
 
@@ -41,72 +41,30 @@
 
 </div>
 	
-<script>$("#addQuestionForm").validate({
-	rules: {
-		question: {
-			required:true,
-			minlength: 3
-		}
-	}
-	});</script>
+<script></script>
 
 <script>
 	 
 $(document).ready(function() {
-	$("#addQuestion").click(function(event) {
-		event.preventDefault();
-		var data = $("#question").serialize();
-		$.ajax({
-			url: "<?php echo base_url(); ?>index.php/questions/addAjax/",
-			type: 'POST',
-			data: data,
-			error: function() {
-				alert("something went wrong");
-			},
-			success: function(response) {
-				addedQuestion = JSON.parse(response);
-
-				var newQuestionHTML = "<div class='questiondiv' id=" + addedQuestion.id + ">" + 
-				"<p class='question' id=" + addedQuestion.id + ">" + 
-				"<span id=" + addedQuestion.id + ">" + addedQuestion.question + "</span>" +
-				" <a href='#' class='edit' id=" + addedQuestion.id + ">edit</a>" +
-				" <a href='#' class='remove' id=" + addedQuestion.id + ">remove</a> </p></div>" +
-				" <span class='editquestion' id=" + addedQuestion.id + " style='display:none'>" + 
-				"<input type='text' name='question' id=" + addedQuestion.id + ">" +
-				"<input type='button' class='btn btn-primary' name='cancel' value='cancel' id=" + addedQuestion.id + ">" +
-				"<input type='submit' class='btn btn-success' name='edit' value='edit' id=" + addedQuestion.id + ">" + "</span>"
-
-				$("#questionList").append(newQuestionHTML);
-				
-			/*	var $cloneDiv = $("#questionList").children().last().clone(true);
-				console.log($cloneDiv);
-				$cloneDiv.children().attr('id', addedQuestion.id);
-				$cloneDiv.attr('id', addedQuestion.id);
-				$cloneDiv.children(".questionspan #"+addedQuestion.id).text(addedQuestion.question);
-				console.log($cloneDiv);
-
-				$("#questionList").append($cloneDiv);
-				*/				
-				
-				/*var newDiv = $("#questionList").append("div");
-				newDiv.addClass('questiondiv').attr('id', addedQuestion.id);
-				var newP = newDiv.append("p");
-				newP.addClass('question').attr('id',addedQuestion.id);
-					
-					
-				newP.append("span").attr('id', addedQuestion.id).text(addedQuestion.question);
-				newP.append("a").addClass('edit').attr('id', addedQuestion.id).attr('href', '#').text('edit');
-				newP.append("a").addClass('remove').attr('id', addedQuestion.id).attr('href', '#').text('remove');
-				var editSpan = newDiv.append("span").addClass('editquestion').attr('id', addedQuestion.id).css('display:none');
-				
-				editSpan.append("input").attr('type','text').attr('name', 'question').attr('id', addedQuestion.id);
-				editSpan.append("input").attr('type', 'button').attr('name', 'cancel').attr('value', 'cancel').attr('id', addedQuestion.id);
-				editSpan.append("input").attr('type', 'button').attr('name', 'edit').attr('value', 'edit').attr('id', addedQuestion.id);
-				*/
-			}
-		});				
-	 });
 	
+	$("#addQuestionForm").validate({
+		rules: {
+			question: {
+				required:true,
+				minlength: 3
+			}
+		}
+	});
+	
+	
+	$("#addQuestionForm").submit(function(event) {
+		event.preventDefault();
+		var isFormValid = $("#addQuestionForm").valid();
+		if (isFormValid) {
+			sendQuestion();
+		}
+	});
+		
 	$("#questionList").on('click', ".remove", function() {
 		var id = $(this).attr('id');
 		$.ajax({
@@ -154,5 +112,56 @@ $(document).ready(function() {
 
 
  });
+
+function sendQuestion() {
+	var data = $("#question").serialize();
+	$.ajax({
+		url: "<?php echo base_url(); ?>index.php/questions/addAjax/",
+		type: 'POST',
+		data: data,
+		error: function() {
+			alert("something went wrong");
+		},
+		success: function(response) {
+			addedQuestion = JSON.parse(response);
+			var newQuestionHTML = "<div class='questiondiv' id=" + addedQuestion.id + ">" + 
+			"<p class='question' id=" + addedQuestion.id + ">" + 
+			"<span id=" + addedQuestion.id + ">" + addedQuestion.question + "</span>" +
+			" <a href='#' class='edit' id=" + addedQuestion.id + ">edit</a>" +
+			" <a href='#' class='remove' id=" + addedQuestion.id + ">remove</a> </p></div>" +
+			" <span class='editquestion' id=" + addedQuestion.id + " style='display:none'>" + 
+			"<input type='text' name='question' id=" + addedQuestion.id + ">" +
+			"<input type='button' class='btn btn-primary' name='cancel' value='cancel' id=" + addedQuestion.id + ">" +
+			"<input type='submit' class='btn btn-success' name='edit' value='edit' id=" + addedQuestion.id + ">" + "</span>"
+			$("#questionList").append(newQuestionHTML);
+				
+		/*	var $cloneDiv = $("#questionList").children().last().clone(true);
+			console.log($cloneDiv);
+			$cloneDiv.children().attr('id', addedQuestion.id);
+			$cloneDiv.attr('id', addedQuestion.id);
+			$cloneDiv.children(".questionspan #"+addedQuestion.id).text(addedQuestion.question);
+			console.log($cloneDiv);
+			$("#questionList").append($cloneDiv);
+			*/				
+			
+			/*var newDiv = $("#questionList").append("div");
+			newDiv.addClass('questiondiv').attr('id', addedQuestion.id);
+			var newP = newDiv.append("p");
+			newP.addClass('question').attr('id',addedQuestion.id);
+				
+					
+			newP.append("span").attr('id', addedQuestion.id).text(addedQuestion.question);
+			newP.append("a").addClass('edit').attr('id', addedQuestion.id).attr('href', '#').text('edit');
+			newP.append("a").addClass('remove').attr('id', addedQuestion.id).attr('href', '#').text('remove');
+			var editSpan = newDiv.append("span").addClass('editquestion').attr('id', addedQuestion.id).css('display:none');
+				
+			editSpan.append("input").attr('type','text').attr('name', 'question').attr('id', addedQuestion.id);
+			editSpan.append("input").attr('type', 'button').attr('name', 'cancel').attr('value', 'cancel').attr('id', addedQuestion.id);
+			editSpan.append("input").attr('type', 'button').attr('name', 'edit').attr('value', 'edit').attr('id', addedQuestion.id);
+			*/
+		},
+	});				
+};
+	
  
 	 </script>
